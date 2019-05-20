3d3a97412c4a88f483c253e6be42b573181db1f1
NohaElMandoh <noha_m_elmandoh@outlook.com>
2019-05-20 14:32:15 -0700

    Apis-cont...

--- app/Http/Controllers/Api/client/MaiController.php
+++ app/Http/Controllers/Api/client/MaiController.php
@@ -2,10 +2,75 @@
 
 namespace App\Http\Controllers\Api\client;
 
+use App\Models\Product;
+use App\Models\Resturant;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class MaiController extends Controller
 {
     //
+    public function new_order(Request $request)
+    {
+//        info(json_encode($request->all()));
+        $validation = validator()->make($request->all(), [
+            'resturant_id' => 'required|exists:resturants,id',
+            'products' => 'required|array',
+            'products.*.product_id' => 'required|exists:products,id',
+            'products.*.count' => 'required',
+            'address' => 'required',
+            'payment_method_id' => 'required|exists:payment_methods,id',
+//            'need_delivery_at' => 'required|date_format:Y-m-d',// H:i:s
+        ]);
+
+        if ($validation->fails()) {
+            $data = $validation->errors();
+            return responseJson(0,$validation->errors()->first(),$data);
+        }
+
+        $resturant = Resturant::find($request->resturant_id);
+        // restaurant status == '0' => closed
+    if($resturant->status == '0')
+    {
+        return responseJson(0,'عذرا المطعم غير متاح في الوقت الحالي');
+    }
+
+//        'cost', 'delivery_cost', 'total', 'commission','net', 'need_delivery_at',
+//        'delivery_time_id', 'delivered_at'
+
+        // client
+        // set defaults
+
+//         'delivery_time', 'delivery_date'
+// 'total_price', 'delivery_status_resturant',  'delivery_status_client',
+//, 'delivery_address', 'notes', 'payment_method_id')'status',;
+
+        $order = $request->user()->make_order()->create([
+            'resturant_id' => $request->resturant_id,
+            'notes' => $request->note,
+            'state' => 'pending', // db default
+            'delivery_address' => $request->address,
+            'payment_method_id' => $request->payment_method_id,
+        ]);
+//
+        $cost = 0;
+        $delivery_cost = $resturant->delivery_fee;
+        foreach ($request->products as $i)
+        {
+            // ['item_id' => 1,'quantity' => 2,'note'=>'no tomato']
+            $product = Product::find($i['product_id']);
+            // item validation // no logic
+            $readyItem = [
+                $i['product_id'] => [
+                    'count' => $i['count'],
+                    'total_price' => $product->price,
+                    'special_order' => (isset($i['note'])) ? $i['note'] : ''
+                ]
+            ];
+            $order->products()->attach($readyItem);
+            $cost += ($product->price * $i['count']);
+        }
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,12 @@ Route::group(['namespace'=>'Api','prefix' =>'v1'],function(){
     Route::get('cities', 'MainController@cities');
     Route::get('neighborhoods', 'MainController@neighborhoods');
     Route::get('neighborhoods_to_city', 'MainController@neighborhoods_to_city');
+    Route::post('resturant_info', 'resturant\MainController@resturant_info');
+    Route::get('about', 'MainController@about');
+    Route::get('contact_us', 'MainController@contact_us');
+    Route::post('resturants', 'MainController@resturants');  ///////// how to search by city
 
+ Route::get('restaurant_products', 'MainController@restaurant_products');
     ///client
     Route::group(['prefix' =>'client'],function(){
         Route::post('register_client', 'client\AuthController@register');
@@ -31,6 +36,8 @@ Route::group(['namespace'=>'Api','prefix' =>'v1'],function(){
         Route::post('reset', 'Client\AuthController@reset');
         Route::post('check_code', 'Client\AuthController@check_code');
         Route::post('update_password', 'Client\AuthController@update_password');
+        Route::post('new_order', 'Client\MaiController@new_order');
+
 
 
         Route::group(['middleware'=>'auth:restaurant'],function(){
@@ -49,7 +56,7 @@ Route::group(['namespace'=>'Api','prefix' =>'v1'],function(){
 
         Route::group(['middleware'=>'auth:restaurant'],function(){
 
-            Route::post('resturant_info', 'resturant\MainController@resturant_info');
+
             Route::post('add_product', 'resturant\MainController@add_product');
             Route::post('all_products', 'resturant\MainController@all_products');
             Route::post('edit_product', 'resturant\MainController@edit_product');
